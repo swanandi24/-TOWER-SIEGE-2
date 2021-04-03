e70a951f8a06e2d3464fd8fc07d2095002b3d332
swanandi24 <73694324+swanandi24@users.noreply.github.com>
2021-04-03 15:46:19 +0530

    Add files via upload

---
+++ js/Block.js
@@ -0,0 +1,33 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+        
+            restitution :0.4,
+            friction :0.0,
+            this.Visiblity = 255
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+        if(this.body.speed < 3){
+          super.display();
+         }
+         else{
+           World.remove(world, this.body);
+           push();
+           this.Visiblity = this.Visiblity - 5;
+           tint(255,this.Visiblity);
+           pop(); 
+         }
+        }
